5d377a9b5b77739a07d353d5c0084cfe57aaf7d2
marcus <marcus.chui.ca@icloud.com>
2025-01-25 19:02:05 -0500

    feat(repl): add CLI command handling and new commands
    - Implement command handling with a structured `cliCommand` type.
    - Add `help` and `exit` commands with respective functionality.
    - Improve command parsing logic in REPL for better UX.

--- repl.go
+++ repl.go
@@ -9,6 +9,7 @@ import (
 
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
@@ -20,12 +21,57 @@ func startRepl() {
 
 		commandName := words[0]
 
-		fmt.Printf("Your command was: %s\n", commandName)
+		if cmd, cmdExists := commands[commandName]; cmdExists {
+			if err := cmd.callback(); err != nil {
+				fmt.Printf("Error executing command: %s\n", err)
+			}
+		} else {
+			fmt.Printf("Unknown command: %s\n", commandName)
+		}
+
 	}
 }
 
+// cleanInput processes a string by converting it to lowercase, splitting it into words, and returning a slice of words.
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+// commandHelp displays a help message with descriptions of available commands and their usage.
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+// commandExit terminates the program with a successful exit status and returns nil error.
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
+// cliCommand represents a single CLI command with its name, description, and a callback function to execute it.
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// commands is a map of supported CLI commands, mapping command names to their corresponding cliCommand struct.
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
 }
