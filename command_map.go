3183b4c85da3c609626a3faaa9764deb0f082bed
marcus <marcus.chui.ca@icloud.com>
2025-01-26 01:04:22 -0500

    refactor(http): extract HTTP GET logic into reusable function
    refactor(command-map): simplify forward and backward navigation by reusing the new HTTP GET utility

--- command_map.go
+++ command_map.go
@@ -17,17 +17,10 @@ type location struct {
 	} `json:"results"`
 }
 
-func commandMapForward(cfg *config) error {
-	var curURL string
-	if cfg.nextLocationsURL != nil {
-		curURL = *cfg.nextLocationsURL
-	} else {
-		curURL = "https://pokeapi.co/api/v2/location-area"
-	}
-
-	res, err := http.Get(curURL)
+func httpGET(url string) (location, error) {
+	res, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("%w", err)
+		return location{}, fmt.Errorf("failed to perform HTTP GET request to %s: %w", url, err)
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
@@ -37,19 +30,36 @@ func commandMapForward(cfg *config) error {
 	}(res.Body)
 
 	if res.StatusCode > http.StatusOK {
-		return fmt.Errorf("HTTP status code %d", res.StatusCode)
+		return location{}, fmt.Errorf("received unexpected HTTP status code %d from %s", res.StatusCode, url)
 	}
 
 	var locations location
 	decoder := json.NewDecoder(res.Body)
+
 	if err := decoder.Decode(&locations); err != nil {
-		return fmt.Errorf("%w", err)
+		return location{}, fmt.Errorf("failed to decode response body: %w", err)
+	} else {
+		return locations, nil
+	}
+}
+
+func commandMapForward(cfg *config) error {
+	var curURL string
+	if cfg.nextLocationsURL != nil {
+		curURL = *cfg.nextLocationsURL
+	} else {
+		curURL = "https://pokeapi.co/api/v2/location-area"
 	}
 
-	cfg.nextLocationsURL = locations.Next
-	cfg.prevLocationsURL = locations.Previous
+	data, err := httpGET(curURL)
+	if err != nil {
+		return err
+	}
 
-	for _, location := range locations.Results {
+	cfg.nextLocationsURL = data.Next
+	cfg.prevLocationsURL = data.Previous
+
+	for _, location := range data.Results {
 		fmt.Println(location.Name)
 	}
 
@@ -62,34 +72,18 @@ func commandMapBack(cfg *config) error {
 		curURL = *cfg.prevLocationsURL
 	} else {
 
-		return fmt.Errorf("no previous page")
+		return fmt.Errorf("you're on the first page")
 	}
 
-	res, err := http.Get(curURL)
+	data, err := httpGET(curURL)
 	if err != nil {
-		return fmt.Errorf("%w", err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Printf("error closing response body: %v", err)
-		}
-	}(res.Body)
-
-	if res.StatusCode > http.StatusOK {
-		return fmt.Errorf("HTTP status code %d", res.StatusCode)
+		return err
 	}
 
-	var locations location
-	decoder := json.NewDecoder(res.Body)
-	if err := decoder.Decode(&locations); err != nil {
-		return fmt.Errorf("%w", err)
-	}
+	cfg.nextLocationsURL = data.Next
+	cfg.prevLocationsURL = data.Previous
 
-	cfg.nextLocationsURL = locations.Next
-	cfg.prevLocationsURL = locations.Previous
-
-	for _, location := range locations.Results {
+	for _, location := range data.Results {
 		fmt.Println(location.Name)
 	}
 
