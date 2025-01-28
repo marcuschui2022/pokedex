f48f57335f0dbfa26dc5ac635b9c5531aea1a007
marcus <marcus.chui.ca@icloud.com>
2025-01-27 20:16:47 -0500

    feat(api): add new HTTP client for PokeAPI integration

---
+++ internal/api/client.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"github.com/marcuschui2022/pokedex/internal/pokecache"
+	"net/http"
+	"time"
+)
+
+const (
+	baseURL = "https://pokeapi.co/api/v2"
+)
+
+type Client struct {
+	httpClient *http.Client
+	cache      pokecache.Cache
+}
+
+func NewClient(timeout time.Duration) *Client {
+	return &Client{
+		httpClient: &http.Client{
+			Timeout: timeout,
+		},
+	}
+}
